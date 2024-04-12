eb4d0cfd367ffe310f93964a880b01511b7ad547
willqueen12 <166862614+willqueen12@users.noreply.github.com>
2024-04-12 14:44:43 -0400

    Update drrank_loss.py
    changed to rely on numpy instead of gurobi

--- src/drrank_loss.py
+++ src/drrank_loss.py
@@ -1,35 +1,38 @@
 ##########################################################
 #### Loss functions and estimation functions          ####
 ##########################################################
-import gurobipy as gp
+import numpy as np
 
 def tau(i_j, Pij, Dij):
     """
-    Computed expected tau
+    Computed expected tau using NumPy for faster computation
     Parameters:
     i_j: Coordinates of the Pij
     Pij: Posterior estimates of the probability of observation i being more biased than observation j
     Dij: Pairwise indicators
-    Eij: Pairwise indicators
     """
-    return gp.quicksum(
-                Pij[a]*Dij[a] + Pij[(a[1],a[0])]*Dij[(a[1],a[0])]
-                - Pij[a]*Dij[(a[1],a[0])] - Pij[(a[1],a[0])]*Dij[a]
-                            for a in i_j)
+    a_array = np.array(i_j)
+    tau_sum = np.sum(
+        Pij[a_array[:, 0]] * Dij[a_array[:, 0]] + Pij[a_array[:, 1]] * Dij[a_array[:, 1]]
+        - Pij[a_array[:, 0]] * Dij[a_array[:, 1]] - Pij[a_array[:, 1]] * Dij[a_array[:, 0]]
+    )
+    return tau_sum
 
 
 def dp(i_j, Pij, Dij):
     """
-    Computed expected discordance proportion
+    Computed expected discordance proportion using NumPy
     Parameters:
     i_j: Coordinates of the Pij
     Pij: Posterior estimates of the probability of observation i being more biased than observation j
     Dij: Pairwise indicators
     Eij: Pairwise indicators
     """
-    # Set objective function
-    return gp.quicksum(  
-                        Pij[a]*Dij[(a[1],a[0])] + Pij[(a[1],a[0])]*Dij[a]
-                                for a in i_j)
+    a_array = np.array(i_j)
+    dp_sum = np.sum(
+        Pij[a_array[:, 0]] * Dij[(a_array[:, 1], a_array[:, 0])]
+        + Pij[(a_array[:, 1], a_array[:, 0])] * Dij[a_array[:, 0]]
+    )
+    return dp_sum
 
 
